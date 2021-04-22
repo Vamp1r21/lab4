361430772a2ed6a4b6d01777648624fa6f4e52fd
Vampir <nik.aleks.ru@yandex.ru>
2021-04-22 13:29:35 +0300

    Form

--- source/repos/PythonApplication2/PythonApplication2/PythonApplication2.py
+++ source/repos/PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,6 +1,15 @@
 import pdb
+import re
 pdb.set_trace()
-questions={}
-mail = 'ADRESS'
-text = 'QUEST'
-questions[mail]= text
+questions= {}
+
+mail=input()
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@][\w\.]+[.]\w{2,3}$'
+while not re.search(regex,mail):
+    print("Некорректный email")
+    mail=input()
+text=input()
+if  text=="":
+    print("Некорректный email")
+    mail=input()
+questions[mail]=text
