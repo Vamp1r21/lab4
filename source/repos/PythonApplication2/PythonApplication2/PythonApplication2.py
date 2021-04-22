a61cfe3c4f68c6b8f56d3c867683c5b63d6e70da
Vampir <nik.aleks.ru@yandex.ru>
2021-04-22 14:10:15 +0300

    add user

--- source/repos/PythonApplication2/PythonApplication2/PythonApplication2.py
+++ source/repos/PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,15 +1,43 @@
 import pdb
+import json
 import re
-pdb.set_trace()
-questions= {}
+users=[]
+file = open('data.txt')
+if(file.readline()!=""):
+    with open('data.txt') as json_file:
+            data = json.load(json_file)
+            for p in data:
+                for key in p:
+                    users.append({
+                    key: p[key], 
+                })
+                
 
-mail=input()
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@][\w\.]+[.]\w{2,3}$'
-while not re.search(regex,mail):
-    print("Некорректный email")
-    mail=input()
-text=input()
-if  text=="":
-    print("Некорректный email")
-    mail=input()
-questions[mail]=text
+def userInput(): 
+    print("Введите email")
+    email=input()
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@][\w\.]+[.]\w{2,3}$'
+    while not re.search(regex,email):
+        print("Некорректный email")
+        email=input()
+    print("Введите ФИО")
+    question=input()
+    if  question=="":
+        print("Вы не ввели имя ")
+        question=input()
+    users.append({
+        email: question,
+    })
+
+print("Ввод пользователей")
+userInput()
+with open('data.txt', 'w') as outfile:
+    json.dump(users, outfile)
+
+
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data:
+         for key in p:
+             print(key+"   :   "+p[key]) 
+
